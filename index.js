095840e2cfc17fc5000dc31197fe14888df130a3
Matt Wilber <mwilber@gmail.com>
2017-05-20 00:34:48 -0400

    - Add support for Amazon intents - Add amazon testing event

--- index.js
+++ index.js
@@ -16,27 +16,48 @@ var actions = {
     },
     'TwoPlus':function(){
         responseData.speech = "I know I know. It's "+requestData.params.first.toString()+requestData.params.second.toString();
-    }
+    },
+	'Help':function(){
+		responseData.speech = "I'm confused. Try asking me about my favorite food. Or aske me a math question, I can add numbers.";
+	}
 };
 
 
 exports.handler = function(event, context) {
+
     ParseEventData(event);
-    actions[requestData.action]();
+
+    try{
+		actions[requestData.action]();
+	}catch(e){
+		actions['Help']();
+	}
+	
     context.succeed(responseData);
+	//context.succeed(requestData);
 };
 
 function ParseEventData(pEvent){
     
     // Get the action
-    if(typeof pEvent.result.action === "string"){
+
+	// TODO: Make recursive function to do this
+
+    //if(typeof pEvent.result.action === "string"){
+	if(pEvent.hasOwnProperty('result')){
+		// Google Assistant
         requestData.action = pEvent.result.action;
-    }
+		requestData.params = pEvent.result.parameters;
+    }else if(pEvent.hasOwnProperty('request')){
+		// Amazon Alexa
+		requestData.action = pEvent.request.intent.name;
+		//requestData.params = pEvent.request.intent.slots;
+	}
     
     // Get the parameters
-    if(typeof pEvent.result.parameters === "object"){
-        requestData.params = pEvent.result.parameters;
-    }
+    //if(typeof pEvent.result.parameters === "object"){
+    //    
+    //}
     
 }
 
